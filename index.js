c83d4d2aa52b2ce8746af46af81d15b3463e5f2c
jarod79 <arrijuria.eric@gmail.com>
2021-12-27 14:53:45 +0100

    Gestion form

--- index.js
+++ index.js
@@ -1,15 +1,50 @@
 const express = require('express');
 
 const app = express();
+const nodemailer = require('nodemailer');
+const cors = require('cors');
+require('dotenv').config();
 
-app.get('/characters', async (req, res) => {
-  res.status(404).send('Route not found! ');
+const port = process.env.PORT || 8000;
+app.use(express.json());
+
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true,
+  optionSuccessStatus: 200,
+  maxAge: 3600,
+};
+app.use(cors(corsOptions));
+
+app.post('/', (req, res) => {
+  const { prenom, nom, mail, message } = req.body;
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD,
+    },
+  });
+
+  const mailOptions = {
+    from: 'eric.arrijuria@gmail.com',
+    to: process.env.EMAIL,
+    text: `Nom: ${prenom} ${nom}
+    Email: ${mail} 
+    Message: ${message}`,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(`Email sent:  ${info.response}`);
+    }
+  });
+
+  res.status(200).send();
 });
 
-app.use('/', (req, res) => {
-  res.status(404).send('Route not found! ');
-});
-
-app.listen(5050, () => {
-  console.log('Terra Battle API now available on http://localhost:5050 !');
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
